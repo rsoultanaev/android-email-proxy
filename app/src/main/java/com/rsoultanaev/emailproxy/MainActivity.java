c75368994931828ec7ab8d3e5abdee8241b59f34
Robert Soultanaev <robert.soultanaev@hotmail.com>
2017-12-30 15:14:30 +0200

    Start POP3 server on startup

--- app/src/main/java/com/rsoultanaev/emailproxy/MainActivity.java
+++ app/src/main/java/com/rsoultanaev/emailproxy/MainActivity.java
@@ -1,13 +1,31 @@
 package com.rsoultanaev.emailproxy;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.rsoultanaev.emailproxy.server.Pop3Server;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static class ServerTask extends AsyncTask<Integer, Void, Void> {
+        protected Void doInBackground(Integer... urls) {
+            int port = urls[0];
+            String host = "localhost";
+            Pop3Server pop3Server = new Pop3Server(host, port);
+            pop3Server.start();
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // AsyncTask not recommended for long running tasks,
+        // so this is for prototyping purposes
+        ServerTask serverTask = new ServerTask();
+        serverTask.execute(27000);
     }
 }
