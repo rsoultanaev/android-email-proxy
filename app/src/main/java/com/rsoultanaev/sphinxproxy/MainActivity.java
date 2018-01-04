55febeaed417c403d984aa3f1ff9ada4efc3dfd4
Robert Soultanaev <robert.soultanaev@hotmail.com>
2018-01-04 18:09:34 +0200

    Start ProxyService in foreground (#5)

--- app/src/main/java/com/rsoultanaev/sphinxproxy/MainActivity.java
+++ app/src/main/java/com/rsoultanaev/sphinxproxy/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.rsoultanaev.sphinxproxy.server.Pop3Server;
 
@@ -27,39 +26,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        serviceStarted = false;
-
-        // AsyncTask not recommended for long running tasks,
-        // so this is for prototyping purposes
-        Pop3Task pop3Task = new Pop3Task();
-        pop3Task.execute(27000);
     }
 
     public void startProxy(View view) {
-        if (!serviceStarted) {
-            Intent proxyIntent = new Intent(this, ProxyService.class);
-            proxyIntent.putExtra("smtpPort", 28000);
-
-            startService(proxyIntent);
-            serviceStarted = true;
-
-            Toast.makeText(this, "Service started", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Service already started", Toast.LENGTH_SHORT).show();
-        }
+        Intent proxyIntent = new Intent(this, ProxyService.class);
+        proxyIntent.putExtra("smtpPort", 28000);
+        startService(proxyIntent);
     }
 
     public void stopProxy(View view) {
-        if (serviceStarted) {
-            Intent proxyIntent = new Intent(this, ProxyService.class);
-
-            stopService(proxyIntent);
-            serviceStarted = false;
-
-            Toast.makeText(this, "Service stopped", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Service already stopped", Toast.LENGTH_SHORT).show();
-        }
+        Intent proxyIntent = new Intent(this, ProxyService.class);
+        stopService(proxyIntent);
     }
 }
--- app/src/main/java/com/rsoultanaev/sphinxproxy/ProxyService.java
+++ app/src/main/java/com/rsoultanaev/sphinxproxy/ProxyService.java
@@ -1,8 +1,11 @@
 package com.rsoultanaev.sphinxproxy;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 
 import com.rsoultanaev.sphinxproxy.server.SmtpMessageHandler;
 
@@ -12,20 +15,41 @@ import org.subethamail.smtp.server.SMTPServer;
 public class ProxyService extends Service {
 
     private SMTPServer smtpServer;
+    private boolean running;
+
+    @Override
+    public void onCreate() {
+        running = false;
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        final int smtpPort = intent.getIntExtra("smtpPort", 28000);
+        if (!running) {
+            final int smtpPort = intent.getIntExtra("smtpPort", 28000);
 
-        // Initialising SMTPServer blocks, so we need to do it in a separate thread
-        new Thread(new Runnable() {
-            public void run() {
-                SmtpMessageHandler smtpMessageHandler = new SmtpMessageHandler();
-                smtpServer = new SMTPServer(new SimpleMessageListenerAdapter(smtpMessageHandler));
-                smtpServer.setPort(smtpPort);
-                smtpServer.start();
-            }
-        }).start();
+            // Initialising SMTPServer blocks, so we need to do it in a separate thread
+            new Thread(new Runnable() {
+                public void run() {
+                    SmtpMessageHandler smtpMessageHandler = new SmtpMessageHandler();
+                    smtpServer = new SMTPServer(new SimpleMessageListenerAdapter(smtpMessageHandler));
+                    smtpServer.setPort(smtpPort);
+                    smtpServer.start();
+                }
+            }).start();
+
+            Intent notificationIntent = new Intent(this, MainActivity.class);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                    notificationIntent, 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setContentTitle("My Awesome App")
+                    .setContentText("Doing some work...")
+                    .setContentIntent(pendingIntent).build();
+
+            startForeground(1337, notification);
+        }
+        running = true;
 
         // TODO: investigate which return is best here
         return Service.START_NOT_STICKY;
@@ -34,6 +58,8 @@ public class ProxyService extends Service {
     @Override
     public void onDestroy() {
         smtpServer.stop();
+        running = false;
+        stopForeground(true);
     }
 
     @Override
