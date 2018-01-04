f411efb7e10d936ff48ba8b7a826bc04a230040f
Robert Soultanaev <robert.soultanaev@hotmail.com>
2018-01-04 12:19:25 +0200

    Add button to start smtp service

--- app/src/main/java/com/rsoultanaev/sphinxproxy/MainActivity.java
+++ app/src/main/java/com/rsoultanaev/sphinxproxy/MainActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.rsoultanaev.sphinxproxy.server.Pop3Server;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean serviceStarted;
 
     private static class Pop3Task extends AsyncTask<Integer, Void, Void> {
         protected Void doInBackground(Integer... urls) {
@@ -24,13 +28,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        serviceStarted = false;
+
         // AsyncTask not recommended for long running tasks,
         // so this is for prototyping purposes
         Pop3Task pop3Task = new Pop3Task();
         pop3Task.execute(27000);
+    }
 
-        Intent startProxyIntent = new Intent(this, ProxyService.class);
-        startProxyIntent.putExtra("smtpPort", 28000);
-        startService(startProxyIntent);
+    public void startSmtp(View view) {
+        if (!serviceStarted) {
+            Intent startProxyIntent = new Intent(this, ProxyService.class);
+            startProxyIntent.putExtra("smtpPort", 28000);
+            startService(startProxyIntent);
+            serviceStarted = true;
+        } else {
+            Toast.makeText(this, "Service already running", Toast.LENGTH_SHORT).show();
+        }
     }
 }
