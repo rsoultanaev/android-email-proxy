a24ee1eea3b8b740053d531f152039c0064156c7
rsoultanaev <robert.soultanaev@hotmail.com>
2018-01-28 17:00:42 +0000

    Use keyId instead of the key string

--- app/src/main/java/com/robertsoultanaev/sphinxproxy/Config.java
+++ app/src/main/java/com/robertsoultanaev/sphinxproxy/Config.java
@@ -4,8 +4,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class Config {
-    public static void setKey(String key, String value, Context context) {
+    public static void setKey(int keyId, String value, Context context) {
         String sharedPreferencesFile = context.getString(R.string.key_preference_file);
+        String key = context.getString(keyId);
         SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPreferencesFile, Context.MODE_PRIVATE);
 
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -13,8 +14,9 @@ public class Config {
         editor.apply();
     }
 
-    public static String getKey(String key, Context context) {
+    public static String getKey(int keyId, Context context) {
         String sharedPreferencesFile = context.getString(R.string.key_preference_file);
+        String key = context.getString(keyId);
         SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPreferencesFile, Context.MODE_PRIVATE);
 
         return sharedPreferences.getString(key, null);
