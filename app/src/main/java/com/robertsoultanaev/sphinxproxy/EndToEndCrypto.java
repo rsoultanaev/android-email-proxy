7eaad99765cdb1d4d475382b8b93ed427797ea22
rsoultanaev <robert.soultanaev@hotmail.com>
2018-02-01 21:21:46 +0000

    Refactor

--- app/src/main/java/com/robertsoultanaev/sphinxproxy/EndToEndCrypto.java
+++ app/src/main/java/com/robertsoultanaev/sphinxproxy/EndToEndCrypto.java
@@ -6,34 +6,45 @@ import java.security.Provider;
 import java.security.SecureRandom;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 public class EndToEndCrypto {
-    public final static int TAG_BIT_LENGTH = 128;
-    public final static int IV_BYTE_LENGTH = 12;
-    public final static int KEY_BYTE_LENGTH = 16;
-    public final static String TRANSFORMATION = "AES/GCM/NoPadding";
-    public final static Provider BC_PROVIDER = new BouncyCastleProvider();
+    private final static Provider BC_PROVIDER = new BouncyCastleProvider();
+
+    private final static int SYMMETRIC_KEY_LENGTH = 128;
+    private static String SYMMETRIC_ALGORITHM_NAME = "AES";
+    private final static String SYMMETRIC_TRANSFORMATION = "AES/GCM/NoPadding";
+    private final static int AES_GCM_TAG_LENGTH = 128;
+    private final static int AES_GCM_IV_LENGTH = 96;
 
     public static SecretKey generateSymmetricKey() {
-        byte[] key = new byte[KEY_BYTE_LENGTH];
-        SecureRandom random = new SecureRandom();
-        random.nextBytes(key);
-        return new SecretKeySpec(key, EndToEndCrypto.TRANSFORMATION);
+        KeyGenerator keyGenerator;
+        SecretKey secretKey;
+
+        try {
+            keyGenerator = KeyGenerator.getInstance(SYMMETRIC_ALGORITHM_NAME, BC_PROVIDER);
+            keyGenerator.init(SYMMETRIC_KEY_LENGTH);
+            secretKey = keyGenerator.generateKey();
+        } catch (Exception ex) {
+            throw new RuntimeException("Key gen failed", ex);
+        }
+
+        return secretKey;
     }
 
     public static GcmEncryptionResult symmetricEncrypt(SecretKey key, byte[] plainText) {
-        byte[] iv = new byte[IV_BYTE_LENGTH];
+        int ivByteLength = AES_GCM_IV_LENGTH / 8;
+        byte[] iv = new byte[ivByteLength];
         SecureRandom random = new SecureRandom();
         random.nextBytes(iv);
-        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(TAG_BIT_LENGTH, iv);
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(AES_GCM_TAG_LENGTH, iv);
 
         Cipher aesGcm;
         byte[] cipherText;
         try {
-            aesGcm = Cipher.getInstance(TRANSFORMATION, BC_PROVIDER);
+            aesGcm = Cipher.getInstance(SYMMETRIC_TRANSFORMATION, BC_PROVIDER);
             aesGcm.init(Cipher.ENCRYPT_MODE, key, gcmParameterSpec);
             cipherText = new byte[aesGcm.getOutputSize(plainText.length)];
             aesGcm.doFinal(plainText, 0, plainText.length, cipherText);
@@ -45,12 +56,12 @@ public class EndToEndCrypto {
     }
 
     public static byte[] symmetricDecrypt(SecretKey key, GcmEncryptionResult gcmEncryptionResult) {
-        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(TAG_BIT_LENGTH, gcmEncryptionResult.iv);
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(AES_GCM_TAG_LENGTH, gcmEncryptionResult.iv);
 
         Cipher aesGcm;
         byte[] plainText;
         try {
-            aesGcm = Cipher.getInstance(TRANSFORMATION, BC_PROVIDER);
+            aesGcm = Cipher.getInstance(SYMMETRIC_TRANSFORMATION, BC_PROVIDER);
             aesGcm.init(Cipher.DECRYPT_MODE, key, gcmParameterSpec);
             plainText = aesGcm.doFinal(gcmEncryptionResult.cipherText, 0, gcmEncryptionResult.cipherText.length);
         } catch (Exception ex) {
