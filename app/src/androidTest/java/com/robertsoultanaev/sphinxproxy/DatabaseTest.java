4fda97394ba610f7bb743a2d59b5bd8133fb1d85
Robert Soultanaev <robert.soultanaev@hotmail.com>
2018-02-07 19:13:36 +0000

    Test database (#28)
    * Add database test skeleton
    
    * Fix incrementing the wrong packetcount value
    
    * Add equals() methods to entities
    
    * Test adding packets and assembled messages
    
    * Add test for deletion
    
    * Add test for ignoring duplicates
    
    * Add mix node test
    
    * Add recipient test

---
+++ app/src/androidTest/java/com/robertsoultanaev/sphinxproxy/DatabaseTest.java
@@ -0,0 +1,149 @@
+package com.robertsoultanaev.sphinxproxy;
+
+import android.arch.persistence.room.Room;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.robertsoultanaev.sphinxproxy.database.AssembledMessage;
+import com.robertsoultanaev.sphinxproxy.database.DB;
+import com.robertsoultanaev.sphinxproxy.database.DBQuery;
+import com.robertsoultanaev.sphinxproxy.database.MixNode;
+import com.robertsoultanaev.sphinxproxy.database.Packet;
+import com.robertsoultanaev.sphinxproxy.database.Recipient;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(AndroidJUnit4.class)
+public class DatabaseTest {
+    private DB db;
+    private DBQuery dbQuery;
+
+    @Before
+    public void createDb() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        db = Room.inMemoryDatabaseBuilder(context, DB.class).build();
+        dbQuery = db.getDao();
+    }
+
+    @After
+    public void closeDb() throws IOException {
+        db.close();
+    }
+
+    @Test
+    public void packetsAndAssembledMessageTest() throws Exception {
+        String messageId = UUID.randomUUID().toString();
+        String packet1Body = "hello\r\n";
+        String packet2Body = "bye\r\n";
+
+        Packet packet1 = new Packet(messageId, 0, 2, packet1Body.getBytes());
+        Packet packet2 = new Packet(messageId, 1, 2, packet2Body.getBytes());
+
+        dbQuery.addPacket(packet1);
+
+        List<Packet> queryResult1 = dbQuery.getPackets(messageId);
+        assertThat(queryResult1.size(), is(1));
+        assertThat(queryResult1.get(0), is(packet1));
+
+        List<String> queryResult2 = dbQuery.getReadyPacketIds();
+        assertThat(queryResult2.isEmpty(), is(true));
+
+        dbQuery.addPacket(packet2);
+
+        List<Packet> queryResult3 = dbQuery.getPackets(messageId);
+        assertThat(queryResult3.size(), is(2));
+        assertThat(queryResult3.get(0), is(packet1));
+        assertThat(queryResult3.get(1), is(packet2));
+
+        List<String> queryResult4 = dbQuery.getReadyPacketIds();
+        assertThat(queryResult4.size(), is(1));
+        assertThat(queryResult4.get(0), is(messageId));
+
+        AssembledMessage assembledMessage = new AssembledMessage(messageId, (packet1Body + packet2Body).getBytes());
+
+        dbQuery.addAssembledMessage(assembledMessage);
+
+        List<Packet> queryResult5 = dbQuery.getPackets(messageId);
+        assertThat(queryResult5.isEmpty(), is(true));
+
+        List<String> queryResult6 = dbQuery.getReadyPacketIds();
+        assertThat(queryResult6.isEmpty(), is(true));
+
+        List<AssembledMessage> queryResult7 = dbQuery.getAssembledMessages();
+        assertThat(queryResult7.size(), is(1));
+        assertThat(queryResult7.get(0), is(assembledMessage));
+
+        dbQuery.deleteAssembledMessage(messageId);
+
+        List<AssembledMessage> queryResult8 = dbQuery.getAssembledMessages();
+        assertThat(queryResult8.isEmpty(), is(true));
+    }
+
+    @Test
+    public void ignoreDuplicatePacketsTest() throws Exception {
+        String messageId = UUID.randomUUID().toString();
+        String packet1Body = "hello\r\n";
+        String packet2Body = "bye\r\n";
+
+        Packet packet1 = new Packet(messageId, 0, 2, packet1Body.getBytes());
+        Packet packet2 = new Packet(messageId, 1, 2, packet2Body.getBytes());
+
+        dbQuery.addPacket(packet1);
+        dbQuery.addPacket(packet2);
+
+        List<Packet> queryResult1 = dbQuery.getPackets(messageId);
+        assertThat(queryResult1.size(), is(2));
+        assertThat(queryResult1.get(0), is(packet1));
+        assertThat(queryResult1.get(1), is(packet2));
+
+        List<String> queryResult2 = dbQuery.getReadyPacketIds();
+        assertThat(queryResult2.size(), is(1));
+        assertThat(queryResult2.get(0), is(messageId));
+
+        dbQuery.addPacket(packet1);
+        dbQuery.addPacket(packet2);
+
+        List<Packet> queryResult3 = dbQuery.getPackets(messageId);
+        assertThat(queryResult3.size(), is(2));
+        assertThat(queryResult3.get(0), is(packet1));
+        assertThat(queryResult3.get(1), is(packet2));
+
+        List<String> queryResult4 = dbQuery.getReadyPacketIds();
+        assertThat(queryResult4.size(), is(1));
+        assertThat(queryResult4.get(0), is(messageId));
+    }
+
+    @Test
+    public void insertGetMixNodeTest() throws Exception {
+        MixNode mixNode = new MixNode(8000, "publicKey");
+
+        dbQuery.insertMixNode(mixNode);
+
+        List<MixNode> queryResult1 = dbQuery.getMixNodes();
+        assertThat(queryResult1.size(), is(1));
+        assertThat(queryResult1.get(0), is(mixNode));
+    }
+
+    @Test
+    public void insertGetRecipientTest() throws Exception {
+        String recipientAddress = "fred@mail.com";
+        Recipient recipient = new Recipient(recipientAddress, "publicKey");
+
+        dbQuery.insertRecipient(recipient);
+
+        Recipient queryResult1 = dbQuery.getRecipient(recipientAddress);
+        assertThat(queryResult1, is(recipient));
+    }
+}
+
--- app/src/main/java/com/robertsoultanaev/sphinxproxy/database/Packet.java
+++ app/src/main/java/com/robertsoultanaev/sphinxproxy/database/Packet.java
@@ -4,6 +4,8 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.support.annotation.NonNull;
 
+import java.util.Arrays;
+
 
 @Entity(primaryKeys = {"uuid", "sequenceNumber", "packetsInMessage"})
 public class Packet {
@@ -22,5 +24,18 @@ public class Packet {
         this.sequenceNumber = sequenceNumber;
         this.packetsInMessage = packetsInMessage;
         this.payload = payload;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Packet packet = (Packet) o;
+
+        if (sequenceNumber != packet.sequenceNumber) return false;
+        if (packetsInMessage != packet.packetsInMessage) return false;
+        if (!uuid.equals(packet.uuid)) return false;
+        return Arrays.equals(payload, packet.payload);
     }
 }
