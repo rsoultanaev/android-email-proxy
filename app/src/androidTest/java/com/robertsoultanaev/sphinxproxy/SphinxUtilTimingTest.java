90fbd25de22341e5b58847ce290812bdc293ac88
rsoultanaev <robert.soultanaev@hotmail.com>
2018-02-20 13:57:47 +0000

    Make timing test runnable on the phone

--- app/src/androidTest/java/com/robertsoultanaev/sphinxproxy/SphinxUtilTimingTest.java
+++ app/src/androidTest/java/com/robertsoultanaev/sphinxproxy/SphinxUtilTimingTest.java
@@ -1,5 +1,7 @@
 package com.robertsoultanaev.sphinxproxy;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import com.robertsoultanaev.javasphinx.SphinxParams;
 import com.robertsoultanaev.sphinxproxy.database.MixNode;
 
@@ -7,14 +9,13 @@ import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.util.encoders.Hex;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(AndroidJUnit4.class)
 public class SphinxUtilTimingTest {
 
     class PkiEntry {
@@ -77,6 +78,5 @@ public class SphinxUtilTimingTest {
         }
 
         return result.toString();
-
     }
 }
